e11716967f06cb3919610273aa7112b7a33adbf0
K <b2ancookies@gmail.com>
2021-07-26 10:40:27 +0900

    ㅇㅁㅇ)==)

--- Pokemon_Mystery_Dungeon/Glass.cpp
+++ Pokemon_Mystery_Dungeon/Glass.cpp
@@ -7,8 +7,8 @@ HRESULT Glass::init(float x, float y, float angle)
 	_x = x;
 	_y = y;
 	_body = RectMakeCenter(x * TILEWIDTH + TILEWIDTH / 2, y * TILEHEIGHT + TILEHEIGHT / 2, ITEMSIZE, ITEMSIZE);
-	_name = L"록온안경"; //자신이 던진 도구가 상대방에게 더 잘 맞는다.
-	_itemInfo = L"아이템 설명이 없습니다";
+	_name = L"록온안경";
+	_itemInfo = L"자신이 던진 도구가 상대방에게 더 잘 맞는다";
 	_itemInfoLong = L"아이템 설명이 없습니다";
 	_type = ITEM_HOLD;
 	_num = 19;
